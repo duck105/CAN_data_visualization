598083710f087f12f8dd01bc6096abc2b311d80c
Po-wen Chen <jacky87810@gmail.com>
2020-12-03 16:27:27 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,url_for,request,redirect, make_response
+import random
+import json
+from time import time
+from random import random
+from flask import Flask, render_template, make_response
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format [ Time, Velocity, SOC]
+    Velocity = random()*150  
+    SOC = random()*100
+
+    data = [time() * 1000, Velocity, SOC]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
