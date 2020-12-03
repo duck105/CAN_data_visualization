27c1a5e23d821152849d5ce64ad0eb6d27cbd733
Po-wen <jacky87810@gmail.com>
2020-12-03 23:44:30 +0800

    Add csv data into chart

--- app.py
+++ app.py
@@ -4,22 +4,26 @@ import json
 from time import time
 from random import random
 from flask import Flask, render_template, make_response
+import pandas as pd
+
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def main():
     return render_template('index.html')
 
 
-@app.route('/data', methods=["GET", "POST"])
+@app.route('/data', methods=["GET"])
 def data():
     # Data Format [ Time, Velocity, SOC]
-    Velocity = random()*150  
-    SOC = random()*100
 
-    data = [time() * 1000, Velocity, SOC]
+    df = pd.read_csv("Arduino1_decode.csv")
+    n = 40000
+    diff = 2500
+    datas = [list(a) for a in zip(list(df.iloc[:n:diff, 0]), list(df.iloc[:n:diff, 6]), list(df.iloc[:n:diff, 1]))]
+    print(len(datas))
 
-    response = make_response(json.dumps(data))
+    response = make_response(json.dumps(datas))
 
     response.content_type = 'application/json'
 
